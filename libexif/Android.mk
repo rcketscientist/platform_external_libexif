87abb750774e2ca74ef060d2fb8be0fe948bb06c
Anthony <amandra@gmail.com>
2016-06-04 00:06:23 +0200

    modify for editing in AS and inclusion as submodule

--- libexif/Android.mk
+++ libexif/Android.mk
@@ -22,8 +22,9 @@ include $(CLEAR_VARS)
 
 LOCAL_CLANG := true
 
-LOCAL_C_INCLUDES := \
-    $(TOP)/external/libexif
+# The upstream organization is awful, but not worth diverging
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_SRC_FILES:= \
     exif-byte-order.c \
